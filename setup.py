20a1ad43d1d2a449c64f42bb9f7f79793ca0c22f
Harshit00yadav <hash00usr@gmail.com>
2025-06-21 15:06:01 +0530

    setup file and requirements added

---
+++ setup.py
@@ -0,0 +1,26 @@
+from os import listdir, system
+from sys import platform
+
+contents = listdir(".")
+if ("genv" in contents):
+    print("[ OK ] environment found")
+else:
+    ans = input("do you want to setup in this folder (Y/n):")
+    if ans == 'n':
+        exit()
+    try:
+        system("python -m venv genv")
+        print("[ OK ] Virtual environment setup done!")
+    except Exception as e:
+        print("[ ERROR ]", e)
+
+print("Installing module dependencies...")
+if platform == "linux":
+    print("No dependencies required")
+elif platform == "win32":
+    try:
+        system("pip install -r requirements.txt")
+    except Exception as e:
+        print("[ ERROR ]", e)
+        exit()
+print("--[FINISHED]--")
